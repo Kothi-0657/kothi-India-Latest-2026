6f241ea5703eca1ff81d874353fb57f7d079e221
Kothi-0657 <service@kothiindia.com>
2025-09-25 12:39:35 +0530

    new Changes are made

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -4,21 +4,9 @@ globalThis.__BUILD_MANIFEST = {
   },
   "devFiles": [],
   "ampDevFiles": [],
-  "polyfillFiles": [
-    "static/chunks/f09e5_next_dist_build_polyfills_polyfill-nomodule.js"
-  ],
+  "polyfillFiles": [],
   "lowPriorityFiles": [],
-  "rootMainFiles": [
-    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_a58d1e74._.js",
-    "static/chunks/f09e5_next_dist_compiled_react-dom_f4e17d7f._.js",
-    "static/chunks/f09e5_next_dist_compiled_next-devtools_index_30c79d34.js",
-    "static/chunks/f09e5_next_dist_compiled_45b331c2._.js",
-    "static/chunks/f09e5_next_dist_client_3897c585._.js",
-    "static/chunks/f09e5_next_dist_3409aa74._.js",
-    "static/chunks/f09e5_@swc_helpers_cjs_4cd7e11e._.js",
-    "static/chunks/Documents_Kothiwebsite_kothiindia2026_my-app_a0ff3932._.js",
-    "static/chunks/turbopack-Documents_Kothiwebsite_kothiindia2026_my-app_805c1c5b._.js"
-  ],
+  "rootMainFiles": [],
   "ampFirstPages": []
 };
 globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
