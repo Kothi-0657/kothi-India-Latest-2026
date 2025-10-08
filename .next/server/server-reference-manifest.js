9879ce2ce210763439c06ef0fddac09b1904e723
Kothi-0657 <service@kothiindia.com>
2025-10-08 12:51:08 +0530

    updated

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"SonFV2v048jg1MteMPTP2HtkPOo2gtMpzAohmRvOK18=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
